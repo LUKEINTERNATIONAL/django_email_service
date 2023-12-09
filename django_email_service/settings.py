e5454c6a08044c228760e5fb34719fdf8ba86b70
dominickasanga <dominickasanga@gmail.com>
2023-12-09 08:56:10 +0200

    modified:   django_email_service/settings.py

--- django_email_service/settings.py
+++ django_email_service/settings.py
@@ -139,7 +139,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # settings.py
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = 'mx-10.globemw.net'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
