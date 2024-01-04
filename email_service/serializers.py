7b39d08a95eff99b87961a6ae4f6cccd70725054
dominickasanga <dominickasanga@gmail.com>
2024-01-04 21:36:42 +0200

    modified:   email_service/serializers.py

--- email_service/serializers.py
+++ email_service/serializers.py
@@ -4,4 +4,4 @@ from .models import Sentemails
 class SentemailsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Sentemails
-        fields = ['subject', 'recipient_email', 'date_created', 'date_updated']
+        fields = ['id','subject', 'recipient_email', 'date_created', 'date_updated']
